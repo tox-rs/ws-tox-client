74c308480dbd5c2da02dc36dd50336c7aa53c8e6
Сухарик <suhr@i2pmail.org>
2019-02-09 03:10:11 +0300

    Start the project

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: 'dist'
+  },
+};
